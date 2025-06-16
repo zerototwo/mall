1f8ee5ca0382b0c002e79596c39c077b58835232
Blanche Cérémonie <130890116+Surtalaevateinn@users.noreply.github.com>
2025-06-16 22:32:13 +0800

    Update AdminOrderController.java

--- src/main/java/com/zhang/ssmschoolshop/controller/admin/AdminOrderController.java
+++ src/main/java/com/zhang/ssmschoolshop/controller/admin/AdminOrderController.java
@@ -39,18 +39,18 @@ public class AdminOrderController {
             return "redirect:/admin/login";
         }
 
-        //一页显示几个数据
+
         PageHelper.startPage(pn, 2);
 
-        //查询未发货订单
+
         OrderExample orderExample = new OrderExample();
         orderExample.or().andIssendEqualTo(false);
         List<Order> orderList = orderService.selectOrderByExample(orderExample);
         model.addAttribute("sendOrder", orderList);
 
-        //查询该订单中的商品
+
         for (int i = 0; i < orderList.size(); i++) {
-            //获取订单项中的goodsid
+
             Order order = orderList.get(i);
             OrderItemExample orderItemExample = new OrderItemExample();
             orderItemExample.or().andOrderidEqualTo(order.getOrderid());
@@ -65,20 +65,20 @@ public class AdminOrderController {
                 goodsList.add(goods);
             }
 
-            //根据goodsid查询商品
+
             /*GoodsExample goodsExample = new GoodsExample();
             goodsExample.or().andGoodsidIn(goodsIdList);
             List<Goods> goodsList = goodsService.selectByExample(goodsExample);*/
             order.setGoodsInfo(goodsList);
 
-            //查询地址
+
             Address address = orderService.getAddressByKey(order.getAddressid());
             order.setAddress(address);
 
             orderList.set(i, order);
         }
 
-        //显示几个页号
+
         PageInfo page = new PageInfo(orderList,5);
         model.addAttribute("pageInfo", page);
 
@@ -95,8 +95,8 @@ public class AdminOrderController {
         order.setOrderid(orderid);
         order.setIssend(true);
         orderService.updateOrderByKey(order);
-        // 发送信息给用户 管理员已经发货了
-        // emailService.sendEmailToUser();
+
+
         return "redirect:/admin/order/send";
     }
 
@@ -106,18 +106,18 @@ public class AdminOrderController {
         if (admin == null) {
             return "redirect:/admin/login";
         }
-        //一页显示几个数据
+
         PageHelper.startPage(pn, 2);
 
-        //查询未收货订单
+
         OrderExample orderExample = new OrderExample();
         orderExample.or().andIssendEqualTo(true).andIsreceiveEqualTo(false);
         List<Order> orderList = orderService.selectOrderByExample(orderExample);
         model.addAttribute("sendOrder", orderList);
 
-        //查询该订单中的商品
+
         for (int i = 0; i < orderList.size(); i++) {
-            //获取订单项中的goodsid
+ 
             Order order = orderList.get(i);
             OrderItemExample orderItemExample = new OrderItemExample();
             orderItemExample.or().andOrderidEqualTo(order.getOrderid());
@@ -134,20 +134,20 @@ public class AdminOrderController {
                 goods.setNum(orderItem.getNum());
                 goodsList.add(goods);
             }
-            //根据goodsid查询商品
+
            /* GoodsExample goodsExample = new GoodsExample();
             goodsExample.or().andGoodsidIn(goodsIdList);
             List<Goods> goodsList = goodsService.selectByExample(goodsExample);*/
             order.setGoodsInfo(goodsList);
 
-            //查询地址
+
             Address address = orderService.getAddressByKey(order.getAddressid());
             order.setAddress(address);
 
             orderList.set(i, order);
         }
 
-        //显示几个页号
+
         PageInfo page = new PageInfo(orderList,5);
         model.addAttribute("pageInfo", page);
 
@@ -160,18 +160,18 @@ public class AdminOrderController {
         if (admin == null) {
             return "redirect:/admin/login";
         }
-        //一页显示几个数据
+
         PageHelper.startPage(pn, 2);
 
-        //查询已完成订单
+
         OrderExample orderExample = new OrderExample();
         orderExample.or().andIssendEqualTo(true).andIsreceiveEqualTo(true).andIscompleteEqualTo(true);
         List<Order> orderList = orderService.selectOrderByExample(orderExample);
         model.addAttribute("sendOrder", orderList);
 
-        //查询该订单中的商品
+
         for (int i = 0; i < orderList.size(); i++) {
-            //获取订单项中的goodsid
+
             Order order = orderList.get(i);
             OrderItemExample orderItemExample = new OrderItemExample();
             orderItemExample.or().andOrderidEqualTo(order.getOrderid());
@@ -189,20 +189,20 @@ public class AdminOrderController {
                 goodsList.add(goods);
             }
 
-            //根据goodsid查询商品
+
             /*GoodsExample goodsExample = new GoodsExample();
             goodsExample.or().andGoodsidIn(goodsIdList);
             List<Goods> goodsList = goodsService.selectByExample(goodsExample);*/
             order.setGoodsInfo(goodsList);
 
-            //查询地址
+
             Address address = orderService.getAddressByKey(order.getAddressid());
             order.setAddress(address);
 
             orderList.set(i, order);
         }
 
-        //显示几个页号
+
         PageInfo page = new PageInfo(orderList, 5);
         model.addAttribute("pageInfo", page);
         return "adminOrderComplete";
